5882d47ac7a4900225fec592712f7a5db7d0adae
oceanuse <31952574+oceanuse@users.noreply.github.com>
2022-05-26 16:48:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,12 @@
 # fw876/helloworld
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
 
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 #sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
 #sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
+git clone https://github.com/kenzok8/openwrt-packages.git  package/openwrt-package
+git clone https://github.com/kenzok8/small-package.git  package/openwrt-small
