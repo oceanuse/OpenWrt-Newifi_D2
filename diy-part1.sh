0c2f5b7161f3edb6dca38827a85c6df6dab6ac39
oceanuse <31952574+oceanuse@users.noreply.github.com>
2022-05-26 21:11:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,10 +13,13 @@
 
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-#sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
+# sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
-#sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
-git clone https://github.com/kenzok8/openwrt-packages.git  package/openwrt-package
-git clone https://github.com/kenzok8/small-package.git  package/openwrt-small
+# sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
+# git clone https://github.com/kenzok8/openwrt-packages.git  package/openwrt-package
+# git clone https://github.com/kenzok8/small-package.git  package/openwrt-small
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
